aeaa2b5d240ba2aae3031b47dc79bd0f58eb8b65
Lars Smith Eier <larssmitheier@protonmail.com>
2025-12-11 11:34:17 +0100

    wrapGAppsHook renamed

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       src = ./.;
 
       nativeBuildInputs = with pkgs; [
-        wrapGAppsHook
+        wrapGAppsHook3
         gobject-introspection
         ags.packages.${system}.default
       ];
